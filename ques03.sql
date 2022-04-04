1a15a17bb8d68313f5789bf1f23794fcb6ca61fe
SHIGIN S <85169428+shijinskml@users.noreply.github.com>
2022-04-04 14:33:13 +0530

    Update ques03.sql

--- ques03.sql
+++ ques03.sql
@@ -1,142 +1,72 @@
-/* Q1 */
-
 create database university;
 use university;
 
-create table student(
-sname varchar(20),
-student_number int primary key,
-class int,
-major varchar(3)
-);
+create table course (course_name varchar(50),course_number varchar(10) primary key,credit_hours int,department varchar(10));
+create table student (sname varchar(20),student_number varchar(10) primary key, class int,major varchar(10));
+create table section (section_identifier varchar(10) primary key,course_number varchar(10), foreign key(course_number) references course(course_number),semester varchar(10),year int, instructor varchar(20));
+create table grade_report(student_number varchar(20), foreign key(student_number) references student(student_number), section_identifier varchar(20), grade varchar(20), foreign key(section_identifier) references section(section_identifier));
+create table prerequisite(course_number varchar(20), foreign key(course_number) references course(course_number),prerequisite_number varchar(20));
+insert into student values('Smith','17',1,'CS');
+insert into student values('Brown','8',2,'CS');
+insert into course values('intro to computer science','CS1310',4,'CS');
+insert into course values('DATA STRUCTURES','CS3320',4,'CS');
+insert into course values('DISCRETE MATHEMATICS','MATH2410',3,'MATH');
+insert into course values('DATABASE','CS3380',3,'CS');
+INSERT INTO section VALUES('85','MATH2410','FALL','07','KING');
+INSERT INTO section VALUES('92','CS1310','FALL','07','ANDERSON');
+INSERT INTO section VALUES('102','CS3320','SPRING','08','KNUTH');
+INSERT INTO section VALUES('112','MATH2410','FALL','08','CHANG');
+INSERT INTO section VALUES('119','CS1310','FALL','08','ANDERSON');
+INSERT INTO section VALUES('135','CS3380','FALL','08','STONE');
+INSERT INTO grade_report values('17','112','B');
+INSERT INTO grade_report values('17','119','C');
+INSERT INTO grade_report values('8','85','A');
+INSERT INTO grade_report values('8','92','A');
+INSERT INTO grade_report values('8','102','B');
+INSERT INTO grade_report values('8','135','A');
+INSERT INTO prerequisite values('CS3380','CS3320');
+INSERT INTO prerequisite values('CS3380','MATH2410');
+INSERT INTO prerequisite values('CS3320','CS1310');
+
+#select course_number from section where section_identifier in (select section_identifier from grade_report where student_number="17" );
 
 
-create table course(
-course_name varchar(50),
-course_number varchar(15) primary key,
-credit_hours int,
-department varchar(10));
 
-create table section(
-section_identifier int primary key,
-course_number varchar(15),
-semester varchar(10),
-syear int,
-instructor varchar(20),
-foreign key(course_number) references course(course_number)
-on delete cascade
-on update cascade
-);
 
-create table grade_report(
-student_number int,
-section_identifier int,
-grade varchar(2),
-foreign key(student_number) references student(student_number),
-foreign key(section_identifier) references section(section_identifier)
-on delete cascade
-on update cascade
-);
-
-create table prerequisite(
-course_number varchar(15),
-prerequisite_number varchar(20) primary key,
-foreign key(course_number) references course(course_number)
-on delete cascade
-on update cascade
-);
-
-/* Q2 */
-
-insert into student values('Smith',17,1,'CS');
-insert into student values('Brown',8,2,'CS');
-
-insert into course values('Intro to Computer Science','CS1310',4,'CS');
-insert into course values('Data Structures','CS3320',4,'CS');
-insert into course values('Discrete Mathematics','MATH2410',3,'MATH');
-insert into course values('Database','CS3380',3,'CS');
-
-insert into section values(85,'MATH2410','Fall',07,'King');
-insert into section values(92,'CS1310','Fall',07,'Anderson');
-insert into section values(102,'CS3320','Spring',08,'Knuth');
-insert into section values(112,'MATH2410','Fall',08,'Chang');
-insert into section values(119,'CS1310','Fall',08,'Anderson');
-insert into section values(135,'CS3380','Fall',08,'Stone');
-
-insert into grade_report values(17,112,'B');
-insert into grade_report values(17,119,'C');
-insert into grade_report values(8,85,'A');
-insert into grade_report values(8,92,'A');
-insert into grade_report values(8,102,'B');
-insert into grade_report values(8,135,'A');
-
-insert into prerequisite values('CS3380', 'CS3320');
-insert into prerequisite values('CS3380', 'MATH2410');
-insert into prerequisite values('CS3320', 'CS1310');
-
-#3. Retrieve the list of  all courses and grades of ‘Smith’
-
-select course.course_name, grade_report.grade 
-from grade_report join course join student 
-where student.major=course.department and grade_report.student_number=student.student_number and student.sname="Smith";
+#3. Retrieve the list of all courses and gradesof‘Smith’
+SELECT student.s_name,grade_report.g_grade,course.c_name FROM student INNER JOIN grade_report ON student.s_no=grade_report.g_stud_no INNER JOIN section ON grade_report.g_sec_identiy=section.s_identity INNER JOIN  course ON section.s_cno=course.c_no WHERE student.s_name='smith';
 
 #4. List the names of students who took the section of the ‘Database’ course offered in fall 2008 and their grades in that section.
-
-select student.sname, grade_report.grade, course.course_name, section.semester, section.syear 
-from student join grade_report join section join course
-where student.student_number=grade_report.student_number and 
-grade_report.section_identifier=section.section_identifier and
-course.course_number=section.course_number and
-course.course_name="Database" and
-section.semester="Fall" and
-section.syear=08;
+SELECT student.s_name,grade_report.g_grade,course.c_name FROM student INNER JOIN grade_report ON student.s_no=grade_report.g_stud_no INNER JOIN section ON grade_report.g_sec_identiy=section.s_identity INNER JOIN  course ON section.s_cno=course.c_no WHERE course.c_name='Database' AND section.s_sem='fall' AND section.s_year='08';
 
 #5. List the prerequisites of the ‘Database’ course.	
-
-select prerequisite.prerequisite_number, course.course_name 
-from prerequisite join course 
-where prerequisite.course_number=course.course_number and course.course_name="Database";
-
+SELECT prerequisite.p_pre_no FROM prerequisite INNER JOIN course ON prerequisite.p_course_no=course.c_no WHERE course.c_name='Database';
 
 #6. Retrieve the names of all senior students majoring in‘CS’(computerscience).
-
-select name from student where major='CS' and class=2;
-
+select s_name from student where s_major='CS' and s_class=4;
 
 #7. Retrieve the names of all courses taught by Professor King in 2007 and 2008.
-SELECT course.course_name
-FROM section INNER JOIN course ON section.course_number=course.course_number
-WHERE section.instructor='king' AND section.year IN('07','08'); 
+SELECT course.c_name FROM section INNER JOIN course ON section.s_cno=course.c_no WHERE section.s_instructor='king' AND section.s_year IN('07','08'); 
 
 #8. For each section taught by Professor King, retrieve the course number,semester, year, and number of students who took the section
-SELECT section.course_number,section.semester,section.year,count(DISTINCT grade_report.student_number) 
-FROM section INNER JOIN grade_report ON grade_report.section_identifier=section.section_identifier
-WHERE section.section_identifier='king' GROUP BY grade_report.section_identifier;
+SELECT section.s_cno,section.s_sem,section.s_year,count(DISTINCT grade_report.g_stud_no) FROM section INNER JOIN grade_report ON grade_report.g_sec_identiy=section.s_identity WHERE section.s_instructor='king' GROUP BY grade_report.g_sec_identiy;
 
 #9. Retrieve the name and transcript of each senior student (Class = 4)majoring in CS. A transcript includes course name, course number, credit hours, semester, year, and grade for each course completed by the student.
-SELECT student.name,course.course_name,course.course_number,course.credit_hours,section.semester,section.year,grade_report.grade
-FROM student 
-INNER JOIN grade_report ON grade_report.student_number=student.student_number
-INNER JOIN section ON section.section_identifier=grade_report.section_identifier 
-INNER JOIN course ON course.course_number=section.course_number
-WHERE student.class=4;
+SELECT student.s_name,course.c_name,course.c_no,course.c_credit,section.s_sem,section.s_year,grade_report.g_grade FROM student INNER JOIN grade_report ON grade_report.g_stud_no=student.s_no INNER JOIN section ON section.s_identity=grade_report.g_sec_identiy INNER JOIN course ON course.c_no=section.s_cno WHERE student.s_class=4;
 
 #10 Write SQL update statements to do the following on the database schema
 #A.Insertanewstudent,<‘Johnson’,25,1,‘Math’>,inthedatabase.
-INSERT INTO student VALUES('Johnson',25,1,'Math');		
+INSERT INTO student VALUES(25,'johnson',1,'Math');		
 SELECT * FROM student;
 								
-#B.Change the class of student ‘Smith’ t o2.
-UPDATE student SET class=2 WHERE name='smith';
+#B.Change th eclass of student ‘Smith’ t o2.
+UPDATE student SET s_class=2 WHERE s_name='smith';
 SELECT * FROM student;
 
 #C.Insertanewcourse,<‘KnowledgeEngineering’,‘CS4390’,3,‘CS’>
-alter table course modify course_name varchar(50);
-
-alter table course modify course_number varchar(10);
-INSERT INTO course VALUES('Knowledge Engineering','4390',3,'CS');
+INSERT INTO course VALUES('CS4390','KnowledgeEngineering',3,'cs');
 SELECT * FROM course;
 
 #D.Delete therecord forthestudentwhose nameis ‘Smith’and whosestudent number is17.
-DELETE FROM student WHERE name='smith' AND student_number=17;
+DELETE FROM student WHERE s_name='smith' AND s_no=17;
 SELECT * FROM student;
